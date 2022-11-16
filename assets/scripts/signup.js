449ed8938635452833acb706795a26eba84c985f
Terry Amagboro <rukkiecodes@gmail.com>
2022-11-16 22:48:54 +0100

    done with page one

--- assets/scripts/signup.js
+++ assets/scripts/signup.js
@@ -10,20 +10,20 @@ const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/ //Minimum eight characters, at least one letter, one number and one special character
 
 // change password type _peepButton starts here
-let type = false;
-_peepButton.addEventListener('click', () => {
-  type = !type
-  const _passwordInput = _peepButton.previousElementSibling.previousElementSibling
-  const _icon = _peepButton.querySelector('i')
+let type = false; // set initial type of input type
+_peepButton.addEventListener('click', () => {//add a click event listener to the (eye) peep button (to be fired when the peep button is clicked)
+  type = !type //negate the value of the input variable
+  const _passwordInput = _peepButton.previousElementSibling.previousElementSibling //get the second siblin element before the peep button
+  const _icon = _peepButton.querySelector('i') //get <i></i> icon element
 
-  _passwordInput.setAttribute('type', type ? 'text' : 'password')
+  _passwordInput.setAttribute('type', type ? 'text' : 'password') // use ternary conditional to toggle input type
 
-  if (type) {
-    _icon.classList.remove('fa-eye')
-    _icon.classList.add('fa-eye-slash')
-  } else {
-    _icon.classList.add('fa-eye')
-    _icon.classList.remove('fa-eye-slash')
+  if (type) { // check if the type variable is true
+    _icon.classList.remove('fa-eye') // remove the fa-eye class from the i element class list
+    _icon.classList.add('fa-eye-slash') // add the fa-eye-slash class from the i element class list
+  } else { // check if the type variable is true
+    _icon.classList.add('fa-eye')// add the fa-eye class from the i element class list
+    _icon.classList.remove('fa-eye-slash')// remove the fa-eye-slash class from the i element class list
   }
 })
 // change password type _peepButton stops here
@@ -48,26 +48,26 @@ _peepButton.addEventListener('click', () => {
 
 
 // validate firstname input on keypress
-_fistname.addEventListener('keypress', () => {
-  let iconDiv = _fistname.previousElementSibling
-  let icon = _fistname.previousElementSibling.querySelector('i')
+_fistname.addEventListener('keypress', () => { // add a keypress event listener to the first name input field (to be fired when you type on the input fiels)
+  let iconDiv = _fistname.previousElementSibling // get the sibmin element before the first name input field <div class="icon">...</div>
+  let icon = _fistname.previousElementSibling.querySelector('i') // get the icon (<i class="..."></i>) tag from the siblin element before the fist name input
 
   if (_fistname.value == '') {//check if first name input is not empty
     // Remove normal classes
-    iconDiv.classList.remove('rais')
-    icon.classList.remove('raisIcon')
-    _fistname.nextElementSibling.classList.remove('activeLabel')
+    iconDiv.classList.remove('rais') // remove the rais class from the i element parent class list
+    icon.classList.remove('raisIcon') // remove the raisIcon class from the icon (<i class="..."></i>) element class list
+    _fistname.nextElementSibling.classList.remove('activeLabel') // remove the activeLabel class from the label (<label>...</label>) element class list
 
     // Add error classes
-    iconDiv.classList.add('raisError')
-    icon.classList.add('raisIconError')
-    _fistname.nextElementSibling.classList.add('activeLabelError')
-    _fistname.classList.add('errorInput')
+    iconDiv.classList.add('raisError') // add the raisError class from the i parent element class list if there is an error
+    icon.classList.add('raisIconError') // add the raisIconError class from the icon (<i class="..."></i>) parent class list if there is an error
+    _fistname.nextElementSibling.classList.add('activeLabelError') // remove the activeLabelError class from the label (<label>...</label>) element class list
+    _fistname.classList.add('errorInput') // add the errorInput class to the first name input field
   }
 })
 
 // validate firstname input on keypress
-_fistname.addEventListener('blur', () => {
+_fistname.addEventListener('blur', () => { // add a blur event listener to the first name input field (to be fired when you click anywhere outside the firstname input field)
   let iconDiv = _fistname.previousElementSibling
   let icon = _fistname.previousElementSibling.querySelector('i')
 
@@ -82,7 +82,7 @@ _fistname.addEventListener('blur', () => {
     icon.classList.add('raisIconError')
     _fistname.nextElementSibling.classList.add('activeLabelError')
     _fistname.classList.add('errorInput')
-  } else {//check if first name input is empty
+  } else {//check if first name input is not empty
     // Remove normal classes
     iconDiv.classList.add('rais')
     icon.classList.add('raisIcon')
@@ -189,11 +189,11 @@ _lastname.addEventListener('blur', () => {
 
 
 // validate email input on keypress
-_emailInput.addEventListener('keypress', () => {
+_emailInput.addEventListener('keypress', () => { // add a keypress event listener to the email input field (to be fired when you type on the input fiels)
   let iconDiv = _emailInput.previousElementSibling
   let icon = _emailInput.previousElementSibling.querySelector('i')
 
-  if (!_emailInput.value.match(emailRegex)) {//check if first name input is not empty
+  if (!_emailInput.value.match(emailRegex)) {//check if the value of the email input is not really a valid email by matching the value with the (emailRegex) variable, using the STRING.match method
     // Remove normal classes
     iconDiv.classList.remove('rais')
     icon.classList.remove('raisIcon')
@@ -204,7 +204,7 @@ _emailInput.addEventListener('keypress', () => {
     icon.classList.add('raisIconError')
     _emailInput.nextElementSibling.classList.add('activeLabelEmailError')
     _emailInput.classList.add('errorInput')
-  } else {
+  } else {//check if the value of the email input is really a valid email by matching the value with the (emailRegex) variable, using the STRING.match method
     // Remove normal classes
     iconDiv.classList.add('rais')
     icon.classList.add('raisIcon')
@@ -223,7 +223,7 @@ _emailInput.addEventListener('blur', () => {
   let iconDiv = _emailInput.previousElementSibling
   let icon = _emailInput.previousElementSibling.querySelector('i')
 
-  if (!_emailInput.value.match(emailRegex)) {//check if first name input is not empty
+  if (!_emailInput.value.match(emailRegex)) {//check if the value of the email input is not really a valid email by matching the value with the (emailRegex) variable, using the STRING.match method
     // Remove normal classes
     iconDiv.classList.remove('rais')
     icon.classList.remove('raisIcon')
@@ -234,7 +234,7 @@ _emailInput.addEventListener('blur', () => {
     icon.classList.add('raisIconError')
     _emailInput.nextElementSibling.classList.add('activeLabelEmailError')
     _emailInput.classList.add('errorInput')
-  } else {//check if first name input is empty
+  } else {//check if the value of the email input is really a valid email by matching the value with the (emailRegex) variable, using the STRING.match method
     // Remove normal classes
     iconDiv.classList.add('rais')
     icon.classList.add('raisIcon')
@@ -277,7 +277,7 @@ _passwordinput.addEventListener('keypress', () => {
   let iconDiv = _passwordinput.previousElementSibling
   let icon = _passwordinput.previousElementSibling.querySelector('i')
 
-  if (!_passwordinput.value.match(passwordRegex)) {//check if first name input is not empty
+  if (!_passwordinput.value.match(passwordRegex)) {//check if the value of the password input is not really a valid password by matching the value with the (passwordRegex) variable, using the STRING.match method
     // Remove normal classes
     iconDiv.classList.remove('rais')
     icon.classList.remove('raisIcon')
@@ -288,7 +288,7 @@ _passwordinput.addEventListener('keypress', () => {
     icon.classList.add('raisIconError')
     _passwordinput.nextElementSibling.classList.add('activeLabelPasswordError')
     _passwordinput.classList.add('errorInput')
-  } else {
+  } else { //check if the value of the password input is really a valid password by matching the value with the (passwordRegex) variable, using the STRING.match method
     // Remove normal classes
     iconDiv.classList.remove('rais')
     icon.classList.remove('raisIcon')
@@ -338,8 +338,23 @@ _passwordinput.addEventListener('blur', () => {
   }
 })
 
-_signupButton.addEventListener('click', () => {
-  if (_fistname.value != '' && _lastname.value != '' && _emailInput.value.match(emailRege) && _passwordinput.value.match(passwordReg) )  {
-    
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// submit form
+_signupButton.addEventListener('click', () => { // add a click event listener to the _signupButton button element
+  if (_fistname.value != '' && _lastname.value != '' && _emailInput.value.match(emailRegex) && _passwordinput.value.match(passwordRegex)) // check if all inputes are valid and are not empty
+    location.replace('dashboard.html')// change web page to dashboard page
+  else
+    _generalError.innerText = 'Please complete the form and try again'// change the text content to the error the form is se to display
 })
